0625cc374df53b4293da746bda7e2ac1d321df4f
acnguyen46 <alan.nguyen@checkmarx.com>
2025-04-03 13:45:55 -0400

    added cypress files

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ...rest of the Cypress project config
+  projectId: "Checkmarx One",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
